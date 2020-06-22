2ecaf092579d1095b0a09ccc5fd59232778366d3
Alexander Verner <Verner.Alx@gmail.com>
2020-06-22 19:54:10 +0500

    Initial Commit

---
+++ UnknownNumber/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  UnknownNumber
+//
+//  Created by Александр Терентьев on 22.06.2020.
+//  Copyright © 2020 AlexanderVerner. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
