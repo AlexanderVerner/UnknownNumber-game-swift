f40097864857d4bd6ed2992d9d105a1d6b6997c1
Alexander Verner <Verner.Alx@gmail.com>
2020-06-24 21:28:10 +0500

    Added a comments in code

--- UnknownNumber/main.swift
+++ UnknownNumber/main.swift
@@ -19,21 +19,31 @@ let message = [
 //Declaring a boolean variable, that will flag for starting new round or closing the game
 var playRound: Bool = true
 repeat {
+//   Show a selected numder message
     print(message["select"]!)
+//   Declaring a random number using pseudo random number function
     let randomNumber = String(arc4random_uniform(10))
+//    Declaring a variable number, that will selected user
     var number : String = ""
     repeat {
+//        Show a "write number" messages with forced extraction
         print(message["start"]!)
+//        Read a numbers, written are users
         let myNumber = readLine()
+//        Safe extraction with using a operator ??
         number = myNumber ?? ""
+//        Checking a written number any more or less guesed computer number
         if number < randomNumber {
             print(message["less"]!)
         } else if number > randomNumber{
             print(message["more"]!)
         }
     } while number != randomNumber
+//    Show a win message
     print(message["win"]!)
+//    Show a try again message
     print(message["try"]!)
+//    Read a command for restart or closing the game
     let restart = readLine()
     if restart == "y"{
         playRound = true
